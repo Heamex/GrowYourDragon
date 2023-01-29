227988b19f5d73a1bba34b67fd87d37b32afc311
kosmo-dev <kuzminvadima@yandex.ru>
2023-01-30 01:56:31 +0400

    Reordered files Added HabitDetailView: - Made layout - Added HabitDetailViewController - Added outlets, actions, keyboard settings

---
+++ Controllers/HabitDetailViewController.swift
@@ -0,0 +1,66 @@
+//
+//  HabitDetailViewController.swift
+//  GrowYourDragon
+//
+//  Created by Вадим Кузьмин on 30.01.2023.
+//
+
+import UIKit
+
+final class HabitDetailViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak private var scrollView: UIScrollView!
+    @IBOutlet weak private var scrollViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    // MARK: - Private variables
+    private var scrollViewBottomConstraintConstant: CGFloat?
+    private var keyboardIsActive: Bool = false
+
+    // MARK: - Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        prepareLayoutForKeyboard()
+    }
+
+    // MARK: - Actions
+    @IBAction private func cancelButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    @IBAction private func saveButtonTapped(_ sender: UIButton) {
+    }
+}
+
+// MARK: - Keyboard Settings
+extension HabitDetailViewController {
+    private func prepareLayoutForKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        scrollViewBottomConstraintConstant = scrollViewBottomConstraint.constant
+    }
+    // Keyboard
+        @objc private func keyboardWillShow(notification: NSNotification) {
+            guard !keyboardIsActive else {
+                return
+            }
+
+            if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                scrollViewBottomConstraint.constant += keyboardSize.height - (scrollViewBottomConstraintConstant ?? 0)
+                keyboardIsActive = true
+            }
+        }
+
+        @objc private func keyboardWillHide(notification: NSNotification) {
+            guard keyboardIsActive else {
+                return
+            }
+
+            scrollViewBottomConstraint.constant = scrollViewBottomConstraintConstant ?? 0
+            keyboardIsActive = false
+        }
+}
