f89c00063b6a050dbd884ab9271e0cf8c5808c16
kosmo-dev <kuzminvadima@yandex.ru>
2023-01-30 21:19:50 +0400

    Made HabitTableViewCell layout

---
+++ Views/HabitTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HabitTableViewCell.swift
+//  GrowYourDragon
+//
+//  Created by Вадим Кузьмин on 30.01.2023.
+//
+
+import UIKit
+
+class HabitTableViewCell: UITableViewCell {
+
+    // MARK: - Outlets
+
+
+    // MARK: - LifeCycle Methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    // MARK: - Actions
+
+    // MARK: - Functions
+}
